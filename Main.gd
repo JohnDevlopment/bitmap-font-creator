cc7734ad4c0ccb7b53350c1d6621e98e667d0313
John Russell <50782633+JohnDevlopment@users.noreply.github.com>
2022-02-24 06:53:40 -0600

    Clear test scene

--- Main.gd
+++ Main.gd
@@ -1,16 +1,3 @@
 extends Control
 
-var VectorControl : Control
-var VectorLabel : Label
-var V : SpinBox
-var H : SpinBox
 
-func _ready() -> void:
-	NodeMapper.map_nodes(self)
-	VectorLabel.text = str(Vector2.ZERO)
-
-func _on_Vector2_value_changed(new_value: Vector2) -> void:
-	VectorLabel.text = str(new_value)
-
-func _on_IntegerProperty_value_changed(value: int) -> void:
-	$Margin/Rows/IntegerProperty/Label.text = str(value)
